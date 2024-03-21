de1f15741d31c1c8cb8fa9929a25c210e49c1f74
Zach <zachalba@uri.edu>
2024-03-21 19:29:49 -0400

    ready to render

--- server.js
+++ server.js
@@ -20,25 +20,46 @@ function serveStaticFile(res, path, contentType, responseCode) {
 
 http.createServer((req, res) => {
     var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
-    var subdirectory = path.split('/')[1];
-    if (subdirectory === 'data') {
-        switch(path){
-            case '/data/data.html':
-                serveStaticFile(res, '/public/data/data.html', 'text/html');
-                break;
-            case '/data/data.xml':
-                serveStaticFile(res, '/public/data/data.xml', 'text/xml');
-                break;
-            case '/data/data.json':
-                serveStaticFile(res, '/public/data/data.json', 'application/json');
-                break;
-            case '/data/jquery_data.html':
-                serveStaticFile(res, '/public/data/jquery_data.html', 'text/html');
-                break;
-        }
-    } else if (subdirectory === 'images') {
-        switch(path){
-            case '/images/logo.png':
+    //console.log(path);
+
+    switch (path) {
+        case '':
+            serveStaticFile(res, '/public/index.html', 'text/html');
+            break;
+        case '/index.html':
+            serveStaticFile(res, '/public/index.html', 'text/html');
+            break;
+        case '/contact.html':
+            serveStaticFile(res, '/public/contact.html', 'text/html');
+            break;
+        case '/about.html':
+            serveStaticFile(res, '/public/about.html', 'text/html');
+            break;
+        case '/FAQ.html':
+            serveStaticFile(res, '/public/FAQ.html', 'text/html');
+            break;
+        case '/services.html':
+            serveStaticFile(res, '/public/services.html', 'text/html');
+            break;
+        case '/resources.html':
+            serveStaticFile(res, '/public/resources.html', 'text/html');
+            break;
+        case '/css/mystyle.css':
+            serveStaticFile(res, '/public/css/mystyle.css', 'text/css');
+            break;
+        case '/data/data.html':
+            serveStaticFile(res, '/public/data/data.html', 'text/html');
+            break;
+        case '/data/data.xml':
+            serveStaticFile(res, '/public/data/data.xml', 'text/xml');
+            break;
+        case '/data/data.json':
+            serveStaticFile(res, '/public/data/data.json', 'application/json');
+            break;
+        case '/data/jquery_data.html':
+            serveStaticFile(res, '/public/data/jquery_data.html', 'text/html');
+            break;
+        case '/images/logo.png':
             serveStaticFile(res, '/public/images/logo.png', 'image/png');
             break;
         case '/images/profile.png':
@@ -56,68 +77,36 @@ http.createServer((req, res) => {
         case '/images/slideshow/relaxing_water.jpg':
             serveStaticFile(res, '/public/images/slideshow/relaxing_water.jpg', 'image/jpeg');
             break;
-        }
-    } else if (subdirectory === 'js') {
-        switch (path){
-            case '/js/animation.js':
-                serveStaticFile(res, '/public/js/animation.js', 'text/javascript');
-                break;
-            case '/js/FAQ.js':
-                serveStaticFile(res, '/public/js/FAQ.js', 'text/javascript');
-                break;
-            case '/js/form.js':
-                serveStaticFile(res, '/public/js/form.js', 'text/javascript');
-                break;
-            case '/js/jq_ajax.js':
-                serveStaticFile(res, '/public/js/jq_ajax.js', 'text/javascript');
-                break;
-            case '/js/jquery.min.js':
-                serveStaticFile(res, '/public/js/jquery.min.js', 'text/javascript');
-                break;
-            case '/js/location.js':
-                serveStaticFile(res, '/public/js/location.js', 'text/javascript');
-                break;
-            case '/js/slideshow.js':
-                serveStaticFile(res, '/public/js/slideshow.js', 'text/javascript');
-                break;
-            case '/js/modernizr.js':
-                serveStaticFile(res, '/public/js/modernizr.js', 'text/javascript');
-                break;
-            case '/js/services.js':
-                serveStaticFile(res, '/public/js/services.js', 'text/javascript');
-                break;
-        }
-    }
-    else{
-    switch (path) {
-        case '':
-            serveStaticFile(res, '/public/index.html', 'text/html');
+        case '/js/animation.js':
+            serveStaticFile(res, '/public/js/animation.js', 'text/javascript');
             break;
-        case '/index':
-            serveStaticFile(res, '/public/index.html', 'text/html');
+        case '/js/FAQ.js':
+            serveStaticFile(res, '/public/js/FAQ.js', 'text/javascript');
             break;
-        case '/contact':
-            serveStaticFile(res, '/public/contact.html', 'text/html');
+        case '/js/form.js':
+            serveStaticFile(res, '/public/js/form.js', 'text/javascript');
             break;
-        case '/about':
-            serveStaticFile(res, '/public/about.html', 'text/html');
+        case '/js/jq_ajax.js':
+            serveStaticFile(res, '/public/js/jq_ajax.js', 'text/javascript');
             break;
-        case '/FAQ':
-            serveStaticFile(res, '/public/FAQ.html', 'text/html');
+        case '/js/jquery.min.js':
+            serveStaticFile(res, '/public/js/jquery.min.js', 'text/javascript');
             break;
-        case '/services':
-            serveStaticFile(res, '/public/services.html', 'text/html');
+        case '/js/location.js':
+            serveStaticFile(res, '/public/js/location.js', 'text/javascript');
             break;
-        case 'resources':
-            serveStaticFile(res, '/public/resources.html', 'text/html');
+        case '/js/slideshow.js':
+            serveStaticFile(res, '/public/js/slideshow.js', 'text/javascript');
             break;
-        case '/css/style.css':
-            serveStaticFile(res, '/public/css/style.css', 'text/css');
+        case '/js/modernizr.js':
+            serveStaticFile(res, '/public/js/modernizr.js', 'text/javascript');
+            break;
+        case '/js/services.js':
+            serveStaticFile(res, '/public/js/services.js', 'text/javascript');
             break;
         default:
             serveStaticFile(res, '/public/404.html', 'text/html', 404);
             break;
-    }
     }
 }).listen(PORT);
 
